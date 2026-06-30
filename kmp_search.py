b60470da505bc60b328d9a21a221f0d2e5a20188
bellinamilito-tech <bellina.milito@gmail.com>
2026-06-30 13:26:13 -0400

    Create kmp_search.py
    Initial KMP addition

---
+++ kmp_search.py
@@ -0,0 +1,55 @@
+def build_lps(pattern):
+    """
+    Build the Longest Prefix Suffix (LPS) array for KMP.
+    LPS[i] = length of the longest proper prefix of pattern[:i+1]
+             which is also a suffix of pattern[:i+1]
+    """
+    lps = [0] * len(pattern)
+    prefix_len = 0  
+    i = 1
+
+    while i < len(pattern):
+        if pattern[i] == pattern[prefix_len]:
+            prefix_len += 1
+            lps[i] = prefix_len
+            i += 1
+        else:
+            if prefix_len != 0:
+                prefix_len = lps[prefix_len - 1]
+            else:
+                lps[i] = 0
+                i += 1
+
+    return lps
+
+
+def kmp_search(text, pattern):
+    """
+    Perform KMP search.
+    Returns a list of starting indices where pattern is found in text.
+    """
+    if not pattern or not text:
+        return []
+
+    lps = build_lps(pattern)
+    matches = []
+
+    i = 0  
+    j = 0  
+
+    while i < len(text):
+        if text[i] == pattern[j]:
+            i += 1
+            j += 1
+
+            
+            if j == len(pattern):
+                matches.append(i - j)
+                j = lps[j - 1] 
+        else:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+
+    return matches
